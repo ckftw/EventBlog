92122142c9d793099df57247bc13c48ea99b57f3
Gaurang Shroff <shroffgaurang9@gmail.com>
2020-03-16 14:49:32 +0530

    completed routes

--- routes/events.js
+++ routes/events.js
@@ -40,9 +40,33 @@ router.post("/", async (req, res) => {
     saveCover(event, req.body.cover)
     try {
         const newEvent = await event.save();
-        res.redirect(`events`);
+        res.redirect(`events/${newEvent.id}`);
     } catch {
         renderNewPage(res, event, true);
+    }
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+    let event
+    try {
+        event =await Event.findById(req.params.id)
+        event.title=req.body.title
+        event.location=req.body.location
+        event.evenDate= new Date(req.body.evenDate)
+        event.cost=req.body.cost
+        event.description = req.body.description
+        if(req.body.cover!=null && req.body.cover!==''){
+            saveCover(event, req.body.cover)
+        }
+        await event.save()
+        res.redirect(`events/${event.id}`);
+    } catch {
+        if(event!== null){
+            renderEditPage(res, event, true);
+        }else{
+            res.redirect('/')
+        }
     }
 });
 
@@ -58,18 +82,59 @@ router.get('/:id', async(req,res)=>{
 })
 
 //edit events
+router.get('/:id/edit', async (req, res) => {
+    try {
+      const event = await Event.findById(req.params.id)
+      renderEditPage(res, event)
+    } catch {
+      res.redirect('/')
+    }
+  })
 
-
+  //delete events
+  router.delete('/:id', async (req, res) => {
+    let event
+    try {
+      event = await Event.findById(req.params.id)
+      await event.remove()
+      res.redirect('/events')
+    } catch {
+      if (event != null) {
+        res.render('events/show', {
+          event: event,
+          errorMessage: 'Could not remove event'
+        })
+      } else {
+        res.redirect('/')
+      }
+    }
+  })
 
 async function renderNewPage(res, event, hasError = false) {
+    renderFormPage(res,event,'new',hasError)
+}
+
+async function renderEditPage(res, event, hasError = false) {
+    renderFormPage(res, event, 'edit', hasError)
+  }
+
+async function renderFormPage(res,event,form , hasError=false){
     try {
         const location = await Location.find({});
         const params = {
             location: location,
             event: event
         };
-        if (hasError) params.errorMessage = "Error creating event";
-        res.render("events/new", params);
+        if(hasError){
+            if(form == 'edit'){
+                params.errorMessage = "Error updating book"
+            }
+            else{
+                params.errorMessage = "Error creating event";
+            }
+        }
+       
+        res.render(`events/${form}`, params);
     } catch {
         res.redirect("/events");
     }
